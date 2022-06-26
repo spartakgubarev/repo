88b2de5cd935a6e19af9bdb3123dd4b246aa61f6
spartakgubarev <spartakgubarev@rambler.ru>
2022-06-26 07:04:18 +0300

    add массивы, методы

---
+++ Example010_MethArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 2, 3, 4, 15, 6, 7, 8, 9, 13, 13, 15, 28 };
+
+int n = array.Length;
+int find = 15;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
