11010e77fb39e5f4274082019632c10e6df328c3
spartakgubarev <spartakgubarev@rambler.ru>
2022-07-05 13:51:06 +0300

    add Examples 011-012 (for, methods, multiplicationTable, array)

---
+++ Example012_MltiplicationTable/Program.cs
@@ -0,0 +1,9 @@
+﻿for (int i = 1; i < 10; i++)
+{
+    Console.Write(i + " ");
+    for (int n = 2; n < 10; n++)
+    {
+        Console.Write(n * i + "      ");
+    }
+    Console.WriteLine();
+}
---
+++ Examples012_Array/Program.cs
@@ -0,0 +1,58 @@
+﻿// Сортировка массива по возрастанию
+// int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+// void PrintArray(int[] array)
+// {
+//     int cout = array.Length;
+//     for (int i = 0; i < cout; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+// void SelectoinSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] < array[minPosition]) minPosition = j;
+//         }
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+//     }
+// }
+// PrintArray(arr);
+// SelectoinSort(arr);
+// PrintArray(arr);
+
+
+// Сортировка массива по убыванию
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+void PrintArray(int[] array)
+{
+    int cout = array.Length;
+    for (int i = 0; i < cout; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void SelectoinSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectoinSort(arr);
+PrintArray(arr);
